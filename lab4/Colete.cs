3e113a97d0269a1a3a957a2cc09d9d5c6e028df2
Sergiu Ciuciu <162841915+SergiuSYS@users.noreply.github.com>
2024-10-11 21:00:43 +0300

    Add files via upload

---
+++ lab4/Colete.cs
@@ -0,0 +1,73 @@
+﻿
+//clasa din care se v-a face mostenirea 
+public abstract class Colet 
+{
+    //declararea variabilelor standard pentru fiecare colet
+    //sunt facute protected incat sa nu poata fi accesate de utilizator in Programul main dar sa fie accesate in alte clase
+    protected string Nume { get; set; }
+    protected int Greutate { get; set; }
+    protected int TimpLivrare;
+    protected int CostPerKg;
+    public Colet(string nume, int greutate)
+    {
+        Nume = nume;
+        Greutate = greutate;
+    }
+    // fecare tip de colet trebuie sa aibe o functie de livrare o facem abstract incat obliga utilizatorul sa creeze una
+    public abstract void Livrare();
+    //functie de calculare a costului total
+    protected int CalculareCostTotal()
+    {
+        return TimpLivrare * CostPerKg;
+    }
+    public override string ToString() => $"{Nume} | {Greutate} KG | {TimpLivrare} zile | {CalculareCostTotal()} EUR";
+}
+
+public class PachetBaza:Colet
+{
+    private int CostTotal;
+    public PachetBaza(string nume, int greutate) : base(nume,greutate)
+    {
+        TimpLivrare = 5;
+        CostPerKg = 5;
+        if (Greutate > 10)
+        {
+            TimpLivrare += 1;
+        }
+    }
+    public override void Livrare()
+    {
+        CostTotal = CalculareCostTotal();
+        Console.WriteLine($"coletul: {Nume} v-a fi livrat in {TimpLivrare} zile, costul livrarii este {CostTotal}");
+    }
+}
+
+public class PachetAvansat : Colet
+{
+    private int CostTotal;
+    public PachetAvansat(string nume, int greutate) : base(nume, greutate)
+    {
+        TimpLivrare = 2;
+        CostPerKg = 6;
+    }
+    public override void Livrare()
+    {
+        CostTotal = CalculareCostTotal()+2;
+        Console.WriteLine($"coletul: {Nume} v-a fi livrat in {TimpLivrare} zile, costul livrarii este {CostTotal}");
+    }
+}
+
+public class PachetPesteNoapte : Colet
+{
+    private int CostTotal;
+    public PachetPesteNoapte(string nume, int greutate) : base(nume, greutate)
+    {
+        TimpLivrare = 1;
+        CostPerKg = 10;
+    }
+    public override void Livrare()
+    {
+        CostTotal = CalculareCostTotal();
+        Console.WriteLine($"coletul: {Nume} v-a fi livrat in {TimpLivrare} zile, costul livrarii este {CostTotal}");
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,60 @@
+﻿int n = 1;
+// am folosit polimorfism
+List<Colet> Colete = new List<Colet>();
+
+while (n != 0) { 
+    Console.WriteLine("=========== MAIN ===========");
+    Console.WriteLine("alegeti una din optiuni: ");
+    Console.WriteLine(" 1. adaugare colet");
+    Console.WriteLine(" 2. afisare lista colete");
+    Console.WriteLine(" 0. iesire");
+    n = int.Parse(Console.ReadLine());
+    switch (n) {
+        case 1:
+            AddColet();
+            break;
+        case 2:
+            AfisareColete();
+            break;
+    }
+}
+
+void AddColet()
+{
+    Console.WriteLine("introdu numele coletului: ");
+    string nume = Console.ReadLine();
+    Console.WriteLine("introdu greutatea coletului: ");
+    int Greutate = int.Parse(Console.ReadLine());
+    Console.WriteLine("Alege tipul coletului: ");
+    Console.WriteLine(" 1. BasicPackage");
+    Console.WriteLine(" 2. AdvancedPackage");
+    Console.WriteLine(" 3. OvernightPackage");
+    int tip = int.Parse(Console.ReadLine());
+
+    Colet colet = null;
+
+    switch (tip)
+    {
+        case 1:
+            colet = new PachetBaza(nume, Greutate);
+            break;
+        case 2:
+            colet = new PachetAvansat(nume, Greutate);
+            break;
+        case 3:
+            colet = new PachetPesteNoapte(nume, Greutate);
+            break;
+        default:
+            Console.WriteLine("Optiune invalida. Nu s-a adaugat niciun colet.");
+            return;
+    }
+    Colete.Add(colet);
+    Console.WriteLine($"{colet} a fost adaugat cu succes!");
+}
+void AfisareColete()
+{
+    foreach(var c in Colete)
+    {
+        Console.WriteLine(c);
+    }
+}
